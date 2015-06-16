684207b90010a1672ce05698e28d732dfbf4a4ba
luckyadam <weitaozsh@gmail.com>
2015-06-16 11:40:34 +0800

    update:loader增加下载时间戳，同时为loader.js增加注释

--- loader.js
+++ loader.js
@@ -4,11 +4,16 @@
  */
 
 var ResourceLoader = (function (global) {
+  'use strict';
 
+  /** 一些基础性方法 */
   var Util = {
+    /** 获取元素类型 */
     getType: function (arg) {
       return Object.prototype.toString.call(arg);
     },
+
+    /** 改变函数执行作用域 */
     proxy: function (fn, context) {
       if (!fn) {
         return;
@@ -18,6 +23,8 @@ var ResourceLoader = (function (global) {
         return fn.apply( context || this, arguments);
       };
     },
+
+    /** 对象和数组的遍历 */
     forEach: function (arg, fn) {
       if (arg.forEach) {
         arg.forEach.call(arg, fn);
@@ -40,7 +47,7 @@ var ResourceLoader = (function (global) {
         }
       }
     },
-    // 仅仅是浅拷贝
+    /** 仅仅是浅拷贝 */
     extend: function (destination, source) {
       for(var key in source) {
         if (source.hasOwnProperty(key)) {
@@ -51,7 +58,9 @@ var ResourceLoader = (function (global) {
     }
   };
 
+  /** 一些状态枚举 */
   var StatusEnum = {
+    /** 整体下载流程中的各种状态 */
     ProcessStatus: {
       NOSTART: 0,
       RUNNING: 1,
@@ -59,11 +68,15 @@ var ResourceLoader = (function (global) {
       COMPLETE: 3,
       ERROR: 4
     },
+
+    /** 执行下载的指令 */
     ProcessCommand: {
       PAUSE: 0,
       STOP: 1,
       NONE: 2
     },
+
+    /** 单个文件的下载状态 */
     FileStatus: {
       NOSTART: 0,
       RUNNING: 1,
@@ -72,6 +85,12 @@ var ResourceLoader = (function (global) {
     }
   };
 
+  /**
+   * Loader - 单个文件下载类
+   *
+   * @param  {Object} [options] 一些参数
+   * @return {Loader}
+   */
   var Loader = function (options) {
     this.conf = Util.extend({
 
@@ -89,16 +108,32 @@ var ResourceLoader = (function (global) {
   Loader.prototype = {
     constructor: Loader,
 
-    _onload: function (e) {
+    /**
+     * _onload - 文件下载完成后的回调函数
+     *
+     * @private
+     */
+    _onload: function () {
       this._status = StatusEnum.FileStatus.COMPLETE;
       this.runHandler(this.handler.onComplete, this._url);
     },
 
-    _onerror: function (e) {
+    /**
+     * _onerror - 文件下载出错的回调函数
+     *
+     * @private
+     */
+    _onerror: function () {
       this._status = StatusEnum.FileStatus.ERROR;
       this.runHandler(this.handler.onError, this._url);
     },
 
+    /**
+     * status - 获取/设置文件下载状态
+     *
+     * @param {Boolean} [status] - 下载状态
+     * @return {Boolean} status
+     */
     status: function () {
       var arg = arguments[0];
       if (arg !== undefined) {
@@ -108,6 +143,13 @@ var ResourceLoader = (function (global) {
       }
     },
 
+    /**
+     * process - 执行文件下载
+     *
+     * @param {Object} param
+     * @param {String} param.url - 文件url
+     * @return {Loader} this
+     */
     process: function (param) {
       this._url = param.url;
       if (!this._url) {
@@ -129,20 +171,44 @@ var ResourceLoader = (function (global) {
         object.onerror = Util.proxy(this._onerror, this);
         this._body.appendChild(object);
       }
+
+      return this;
     },
 
+    /**
+     * bind - 绑定事件
+     *
+     * @param {String} handler - 事件名称
+     * @param {Function} fn - 事件句柄
+     * @return {Loader} this
+     */
     bind: function (handler, fn) {
       this.handler[handler] = fn;
       return this;
     },
 
+    /**
+     * runHandler - 执行事件
+     *
+     * @param {String} handler - 事件名称
+     * @param {Object} param - 参数
+     * @return {Loader} this
+     */
     runHandler: function (handler, param) {
       if (!! handler) {
         handler(param);
       }
+
+      return this;
     }
   };
 
+  /**
+   * LoaderPool - 串行下载池类
+   *
+   * @param  {Object} [options] 一些参数
+   * @return {LoaderPool}
+   */
   var LoaderPool = function (options) {
     this.conf = Util.extend({
     }, options);
@@ -152,24 +218,34 @@ var ResourceLoader = (function (global) {
       onItemComplete: null,
       onItemStart: null,
       onItemError: null,
-      onItemProcessing: null,
       onComplete: null,
       onStart: null,
       onError: null,
       onPause: null,
-      onProcessing: null
     };
   };
 
   LoaderPool.prototype = {
     constructor: LoaderPool,
 
+    /**
+     * _createLoader - 创建单个资源下载器，并绑定相应事件
+     *
+     * @private
+     * @return {Loader} loader
+     */
     _createLoader: function () {
       var loader = new Loader();
       return loader.bind('onComplete', Util.proxy(this._onItemComplete, this))
         .bind('onError', Util.proxy(this._onItemError, this));
     },
 
+    /**
+     * _getLoader - 获取缓存中空闲的下载器，若没有则创建
+     *
+     * @private
+     * @return {Loader} loader
+     */
     _getLoader: function () {
       var ret = null;
       for (var i = 0; i < this._cache.length; i++) {
@@ -188,6 +264,12 @@ var ResourceLoader = (function (global) {
       return ret;
     },
 
+    /**
+     * _onItemComplete - 单个文件下载完成后的回调
+     *
+     * @private
+     * @param {String} item 文件url
+     */
     _onItemComplete: function (item) {
       this.runHandler(this.handler.onItemComplete, item);
       this._complete.push(item);
@@ -196,23 +278,28 @@ var ResourceLoader = (function (global) {
       this._toPause();
     },
 
+    /**
+     * _onItemError - 单个文件下载失败时的回调
+     *
+     * @private
+     * @param {String} item 文件url
+     */
     _onItemError: function () {
-      this.runHandler(this.handler.onItemError, item)
-        .runHandler(this.handler.onError, item);
+      this.runHandler(this.handler.onError, item);
       this._toComplete();
       this._toContinue();
       this._toPause();
     },
 
+    /**
+     * _toComplete - 判断是否下载完成，若下载完成则执行相应句柄
+     *
+     * @private
+     * @return {Boolean} 是否下载完成
+     */
     _toComplete: function () {
-      var isComplete = false;
-      if (this._queue.length === this._complete.length) {
-        for (var i = 0; i < this._queue.length; i++) {
-          this._queue[i] = this._complete[i];
-          isComplete = true;
-        }
-
-        if (isComplete && this._status !== StatusEnum.ProcessStatus.COMPLETE) {
+      if (this._queue.length === 0) {
+        if (this._status !== StatusEnum.ProcessStatus.COMPLETE) {
           this._status = StatusEnum.ProcessStatus.COMPLETE;
           this.runHandler(this.handler.onComplete);
           return true;
@@ -221,14 +308,24 @@ var ResourceLoader = (function (global) {
       return false;
     },
 
+    /**
+     * _toContinue - 判断是否需要继续下载
+     *
+     * @private
+     */
     _toContinue: function () {
       if (this._status !== StatusEnum.ProcessStatus.COMPLETE && this._command == StatusEnum.ProcessCommand.NONE) {
-        this.process({
+        this._process({
           queue: this._queue
         });
       }
     },
 
+    /**
+     * _toPause - 判断是否需要暂停下载
+     *
+     * @private
+     */
     _toPause: function () {
       if (this._command === StatusEnum.ProcessCommand.PAUSE && this._status === StatusEnum.ProcessStatus.RUNNING) {
         this._status = StatusEnum.ProcessStatus.PAUSE;
@@ -236,9 +333,12 @@ var ResourceLoader = (function (global) {
       }
     },
 
-    process: function (param) {
-      this._queue = param.queue;
-      this.runHandler(this.handler.onStart);
+    /**
+     * _process - 执行下载
+     *
+     * @private
+     */
+    _process: function () {
       if (!this._queue) {
         return;
       }
@@ -263,21 +363,61 @@ var ResourceLoader = (function (global) {
       }
     },
 
+    /**
+     * process - 开始执行下载
+     *
+     * @param {Object} param
+     * @param {Array} param.queue - 文件列表
+     * @return {LoaderPool} this
+     */
+    process: function (param) {
+      this._queue = param.queue;
+      this.runHandler(this.handler.onStart);
+      this._process();
+      return this;
+    },
+
+    /**
+     * pause - 暂停下载
+     *
+     * @return {LoaderPool} this
+     */
     pause: function () {
       this._command = StatusEnum.ProcessCommand.PAUSE;
+      return this;
     },
 
+    /**
+     * goOn - 继续下载
+     *
+     * @return {LoaderPool} this
+     */
     goOn: function () {
-      this.process({
+      this._process({
         queue: this._queue
       });
+      return this;
     },
 
+    /**
+     * bind - 绑定事件
+     *
+     * @param {String} handler - 事件名称
+     * @param {Function} fn - 事件句柄
+     * @return {LoaderPool} this
+     */
     bind: function (handler, fn) {
       this.handler[handler] = fn;
       return this;
     },
 
+    /**
+     * runHandler - 执行事件
+     *
+     * @param {String} handler - 事件名称
+     * @param {Object} param - 参数
+     * @return {LoaderPool} this
+     */
     runHandler: function (handler, param) {
       if (!! handler) {
         handler(param);
@@ -286,26 +426,39 @@ var ResourceLoader = (function (global) {
     }
   };
 
+  /** 资源下载器，实例化下载池子 **/
   var _ResourceLoader = {
     load: function (param) {
       var deps = param.deps || null;
-      if (deps === null) {
+      if (deps === null || !deps.length) {
         console.warn('依赖资源列表为空，本次将不预加载任何内容！');
         return;
       }
 
+      var timeStat = {};
       var loaderPool = new LoaderPool();
-      loaderPool.bind('onItemComplete', function (url) {
-        console.log(url + ' 加载完成!');
+
+      // 绑定各种事件，开始执行下载
+      loaderPool.bind('onStart', function () {
+        console.log('开始下载');
+      }).bind('onItemStart', function (url) {
+        var startDate = new Date();
+        var startTimeStamp = startDate.getTime();
+        timeStat[url] = {};
+        timeStat[url].startTime = startTimeStamp;
+      }).bind('onItemComplete', function (url) {
+        var endDate = new Date();
+        var endTimeStamp = endDate.getTime();
+        timeStat[url] && (timeStat[url].endTime = endTimeStamp);
+      }).bind('onItemError', function (url) {
+        console.log('资源 ' + url + '加载失败');
       }).bind('onComplete', function () {
-        console.log('全部加载完成');
-      });
-      loaderPool.process({
+        console.log(timeStat);
+      }).process({
         queue: deps
       });
     }
   };
 
   return _ResourceLoader;
-
 })(window);
